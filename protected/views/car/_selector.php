56263f47f9f8ff588f6fc2ebf9dadeee4e0c5709
Roman <zhukovskey@gmail.com>
2016-01-11 03:17:16 +0300

    car date selector ready

--- protected/views/car/_selector.php
+++ protected/views/car/_selector.php
@@ -2,7 +2,7 @@
 <?php
 /**
  * @var $this CarController
- * @var $model Act
+ * @var $model Car
  * @var $form CActiveForm
  */
 
@@ -31,6 +31,33 @@ $months = [
     'декабрь',
 ];
 
+$ts1 = strtotime($model->from_date);
+$ts2 = strtotime($model->to_date);
+
+$year1 = date('Y', $ts1);
+$year2 = date('Y', $ts2);
+
+$month1 = date('m', $ts1);
+$month2 = date('m', $ts2);
+
+$diff = (($year2 - $year1) * 12) + ($month2 - $month1);
+switch ($diff) {
+    case 1:
+        $period = 1;
+        break;
+    case 3:
+        $period = 2;
+        break;
+    case 6:
+        $period = 3;
+        break;
+    case 12:
+        $period = 4;
+        break;
+    default:
+        $period = 0;
+}
+
 $form = $this->beginWidget('CActiveForm', array(
     'method' => 'get',
     'action' => Yii::app()->createUrl("/car/{$this->action->id}", array('id' => $id)),
@@ -44,11 +71,11 @@ $form = $this->beginWidget('CActiveForm', array(
         <tr>
             <th style="text-align: left;">
                 <?=CHtml::label('Выбор периода', '')?>
-                <?=CHtml::dropDownList('period', 0, Act::$periodList, array('class' =>'select-period autoinput', 'style' => 'margin-right: 10px;'))?>
-                <?=CHtml::dropDownList('year', 10, range(date('Y') - 10, date('Y')), ['class' => 'autoinput', 'style' => 'display:none'])?>
-                <?=CHtml::dropDownList('half', '', $halfs, ['class' => 'autoinput', 'style' => 'display:none'])?>
-                <?=CHtml::dropDownList('quarter', '', $quarters, ['class' => 'autoinput', 'style' => 'display:none'])?>
-                <?=CHtml::dropDownList('month', '', $months, ['class' => 'autoinput', 'style' => 'display:none'])?>
+                <?=CHtml::dropDownList('period', $period, Act::$periodList, array('class' =>'select-period autoinput', 'style' => 'margin-right: 10px;'))?>
+                <?=CHtml::dropDownList('year', 10, range(date('Y') - 10, date('Y')), ['class' => 'autoinput', 'style' => $diff <= 12 ? '' : 'display:none'])?>
+                <?=CHtml::dropDownList('half', '', $halfs, ['class' => 'autoinput', 'style' => $diff == 6 ? '' : 'display:none'])?>
+                <?=CHtml::dropDownList('quarter', '', $quarters, ['class' => 'autoinput', 'style' => $diff == 3 ? '' : 'display:none'])?>
+                <?=CHtml::dropDownList('month', '', $months, ['class' => 'autoinput', 'style' => $diff == 1 ? '' : 'display:none'])?>
                 <?=$form->hiddenField($model, 'from_date', ['class' => 'from_date'])?>
                 <?=$form->hiddenField($model, 'to_date', ['class' => 'to_date'])?>
                 <?=CHtml::submitButton('Показать', array('class' => 'submit radius2 date-send', 'style' => 'opacity: 1;')); ?>
