c8853b5d9bd5c5a60fba2430ee1eb9a7f9112a24
dmitrydeco <dmitrydeco@gmail.com>
2016-03-03 16:42:52 +0300

    small fixes

--- protected/views/car/upload/list.php
+++ protected/views/car/upload/list.php
@@ -5,11 +5,16 @@
  */
 
 $this->renderPartial('_tabs', array('model'=>$model));
-$this->tabs['export'] = ['url' => '#', 'name' => 'Загруженные'];
+
+echo CHtml::link('Назад', Yii::app()->createUrl('car/upload'), array('class' => 'btn-info pull-right', 'style' => 'margin: 0 0 0 20px'));
+echo "<br /><br />"
 ?>
     <div class="contenttitle radiusbottom0">
         <h2 class="table"><span>Добавленные</span></h2>
     </div>
 <?php
-
-$this->renderPartial('_list', array('model'=>$model));
+if (count($model) > 0) {
+    $this->renderPartial('_list', array('model'=>$model));
+} else {
+    echo "Ничего не импортировано";
+}
