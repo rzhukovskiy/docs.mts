8e5c892e6dd836824dd70beee72fb2ce74857d5a
rzhukovskiy <zhukovskey@gmail.com>
2016-03-28 20:31:17 +0300

    default login/pass for demo version

--- protected/views/home/login.php
+++ protected/views/home/login.php
@@ -4,10 +4,10 @@
             <div class="loginerror"><p>Invalid username or password</p></div>
             <form id="login" action="<?php echo Yii::app()->createUrl('home/login') ?>" method="post">
                 <p>
-                    <input id="LoginForm_username" name="LoginForm[username]" type="text" class="radius2" placeholder="login" />
+                    <input id="LoginForm_username" name="LoginForm[username]" value="<?=(strpos($_SERVER['HTTP_HOST'], 'demo') !== false) ? 'сервис' : ''?>" type="text" class="radius2" placeholder="login" />
                 </p>
                 <p>
-                    <input id="LoginForm_password" name="LoginForm[password]" type="password" class="radius2" placeholder="password"  />
+                    <input id="LoginForm_password" name="LoginForm[password]" value="<?=(strpos($_SERVER['HTTP_HOST'], 'demo') !== false) ? '1234' : ''?>" type="password" class="radius2" placeholder="password"  />
                 </p>
                 <p>
                     <button id="formBtn" type="submit" class="radius3 bebas">Войти</button>
